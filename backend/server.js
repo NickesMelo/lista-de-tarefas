b83bc5698488ab18b50f01acfc9a6b51f661ab9f
NickesMelo <nunes.melo18@outlook.com.br>
2025-03-24 11:24:46 -0300

    feat: adicionar integração com SQLite3 e criar tabela 'tasks'

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,43 @@
-const express = require("express");
+const express = require('express');
+const sqlite3 = require('sqlite3').verbose();
 const app = express();
 
+//Conexão com o banco de dados SQLite
+const db = new sqlite3.Database('./database.db', (err) => {
+    if (err) {
+        console.error('Erro ao conectar ao banco de dados', err.message);
+    } else {
+        console.log('Conectado ao banco de dados com sucesso!');
+    }
+});
+
+//Middleware para tratar requisições JSON
 app.use(express.json()); // Permite trabalhar com JSON
 
+//Porta do servidor
+const PORT = 3000;
+
+//Rota para listar todos os produtos (teste da API)
 app.get("/", (req, res) => {
     res.send("Servidor funcionando!");
 });
 
-const PORT = 3000;
+//Criar a tabela 'tasks' se não existir
+db.serialize(() => {
+    db.run(`
+        CREATE TABLE IF NOT EXISTS tasks (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT,
+            description TEXT,
+            status TEXT
+        )
+    `);
+});
+
+
+
+
+//Iniciar o servidor
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 })
